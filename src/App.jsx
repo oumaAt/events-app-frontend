39fd9b961e39f8cb09b5ecaa6a919ab747d5db9a
OUMAYMA <atouie.oumayma@gmail.com>
2025-07-21 03:44:29 +0200

    Initial commit - project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Plateforme de gestion des évènements!</h1>
+    </div>
+  );
+}
